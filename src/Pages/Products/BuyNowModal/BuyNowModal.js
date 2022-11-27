48278115b51d590e56fa74072ffbd4673329cbcf
zahidhasan125 <zahidhasan125@gmail.com>
2022-11-28 05:13:03 +0600

    product added to orders using axios

--- src/Pages/Products/BuyNowModal/BuyNowModal.js
+++ src/Pages/Products/BuyNowModal/BuyNowModal.js
@@ -1,15 +1,37 @@
 import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import { AuthContext } from '../../../contexts/AuthProvider';
+import toast from 'react-hot-toast';
 
 const BuyNowModal = ({ selectedProduct, setSelectedProduct }) => {
     const { name, price } = selectedProduct;
     const { user } = useContext(AuthContext);
     const { register, handleSubmit } = useForm();
+    const navigate = useNavigate();
 
     const handleConfirm = data => {
-        
+        const orderedProduct = { ...data, productId: selectedProduct._id };
+        console.log(orderedProduct);
+
+        axios.post('http://localhost:5000/orders', orderedProduct, {
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${localStorage.getItem('buy-sell-power-tools-token')}`
+            }
+        })
+            .then(res => {
+                console.log(res.data);
+                if (res.data.acknowledged) {
+                    toast.success('This product is added to your orders.');
+                    navigate('/dashboard/myorders');
+                    setSelectedProduct(null);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
     return (
         <>
@@ -22,27 +44,27 @@ const BuyNowModal = ({ selectedProduct, setSelectedProduct }) => {
                             <form onSubmit={handleSubmit(handleConfirm)}>
                                 <div>
                                     <label className='text-sm ml-2' htmlFor="name">Name</label>
-                                    <input type="text" {...register("email")} value={user?.displayName} className="input input-bordered input-sm w-full mb-2 text-base" disabled />
+                                    <input type="text" {...register("userName")} value={user?.displayName} className="input input-bordered input-sm w-full mb-2 text-base" readOnly />
                                 </div>
                                 <div>
                                     <label className='text-sm ml-2' htmlFor="email">Email</label>
-                                    <input type="text" value={user?.email} className="input input-bordered input-sm w-full mb-2 text-base" disabled />
+                                    <input type="text" {...register("userEmail")} value={user?.email} className="input input-bordered input-sm w-full mb-2 text-base" readOnly />
                                 </div>
                                 <div>
                                     <label className='text-sm ml-2' htmlFor="product">Product Name</label>
-                                    <input type="text" value={name} className="input input-bordered input-sm w-full mb-2 text-base" disabled />
+                                    <input type="text" {...register("productName")} value={name} className="input input-bordered input-sm w-full mb-2 text-base" readOnly />
                                 </div>
                                 <div>
                                     <label className='text-sm ml-2' htmlFor="price">Price</label>
-                                    <input type="text" value={`$${price}`} className="input input-bordered input-sm w-full mb-2 text-base" disabled />
+                                    <input type="text" {...register("price")} value={`$${price}`} className="input input-bordered input-sm w-full mb-2 text-base" readOnly />
                                 </div>
                                 <div>
                                     <label className='text-sm ml-2' htmlFor="phone">Phone</label>
-                                    <input type="text" placeholder='+880' className="input input-bordered input-sm w-full mb-2 text-base" />
+                                    <input type="text" {...register("phone")} placeholder='+880' className="input input-bordered input-sm w-full mb-2 text-base" />
                                 </div>
                                 <div>
                                     <label className='text-sm ml-2' htmlFor="location">Meeting Location</label>
-                                    <input type="text" placeholder='Dhaka, Khulna, Chittagong' className="input input-bordered input-sm w-full mb-2 text-base" />
+                                    <input type="text" {...register("location")} placeholder='Dhaka, Khulna, Chittagong' className="input input-bordered input-sm w-full mb-2 text-base" />
                                 </div>
 
                                 <input type="submit" value="Confirm" className='btn btn-sm btn-primary w-full text-white' />
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -2,16 +2,25 @@ import { GoogleAuthProvider } from 'firebase/auth';
 import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider';
+import useToken from '../../hooks/useToken';
 
 const googleProvider = new GoogleAuthProvider();
 
 const SignUp = () => {
     const { createUser, providerLogin, updateUserInfo, logOut } = useContext(AuthContext);
     const [signUpError, setSignUpError] = useState("");
+    const [signUpUserEmail, setSignUpUserEmail] = useState('');
+    const [token] = useToken(signUpUserEmail);
     const { register, formState: { errors }, handleSubmit } = useForm();
     const navigate = useNavigate();
+    const location = useLocation()
+    const from = location.state?.from?.pathname || '/';
+
+    if (token) {
+        navigate(from, {replace: true})
+    }
     const handleSignUp = data => {
         setSignUpError("")
         createUser(data.email, data.password)
@@ -25,6 +34,7 @@ const SignUp = () => {
                     userType: data.userType
 
                 }
+                setSignUpUserEmail(data.email);
                 saveUserToDb(usrInfo);
                 updateUserInfo(userInfo)
                     .then(() => {
@@ -55,7 +65,8 @@ const SignUp = () => {
         fetch('http://localhost:5000/users', {
             method: "POST",
             headers: {
-                'content-type': 'application/json'
+                'content-type': 'application/json',
+                authorization: `Bearer ${localStorage.getItem('buy-sell-power-tools-token')}`
             },
             body: JSON.stringify(user)
         })
